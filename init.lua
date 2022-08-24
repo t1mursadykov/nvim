5b85c9246ad96c7ddd31516e118c6afae66cdd8d
Садыков Тимур <sdkv.tmr@gmail.com>
2022-08-24 20:55:37 +0500

    del some stuff

--- init.lua
+++ init.lua
@@ -7,11 +7,6 @@ require('plugins.telescope')
 require('plugins.which_key')
 
 require('options')
--- Transparent background 
--- TODO: remove from this file
-vim.cmd("highlight Normal guibg=none")
 
---Open tree
-vim.cmd("NvimTreeOpen")
 
 
